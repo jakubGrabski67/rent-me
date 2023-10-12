0f9b917575e9aec6d42542c3731b71315092946a
jakubGrabski67 <kubagrabski22@gmail.com>
2023-10-12 02:28:09 +0200

    Commit frontend and backend 12.10.2023

---
+++ backend/models/Reservation.js
@@ -0,0 +1,89 @@
+const mongoose = require('mongoose');
+
+const reservationSchema = new mongoose.Schema(
+  {
+    // user: {
+    //   type: mongoose.Schema.Types.ObjectId,
+    //   required: true,
+    //   ref: 'Customer',
+    // },
+    car: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'Car', // Zmień na odpowiednią nazwę modelu pojazdu
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+    },
+    protectionPackage: {
+      type: String,
+      required: true,
+    },
+    selectedOptions: [
+      {
+        type: String,
+      },
+    ],
+    selectedPaymentOption: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    street: {
+      type: String,
+      required: true,
+    },
+    houseNumber: {
+      type: String,
+      required: true,
+    },
+    postalCode: {
+      type: String,
+      required: true,
+    },
+    driverLicenseNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    promoCode: {
+      type: String,
+    },
+    reservationStatus: {
+      type: String,
+      default: false
+  },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Reservation', reservationSchema);
